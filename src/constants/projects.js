b257e94746a7f05b4305be6c961bdf55d85f2dbc
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-11 14:46:47 -0300

    feat: Projects data added to contants

---
+++ src/constants/projects.js
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    name: 'Countries infos',
+    description: 'Um site para visualizar informações de países do mundo',
+    site: 'https://countries-infos.onrender.com/',
+    github: 'https://github.com/Josimar-Souza/countries-info',
+    imagePath: '../../images/projects/countries-infos.png',
+  },
+  {
+    name: 'Sharp Movies',
+    description: 'Um site para visualizar filmes e séries',
+    site: 'https://sharp-movies.onrender.com/movies',
+    github: 'https://github.com/Josimar-Souza/movies-site',
+    imagePath: '../../images/projects/sharp-movies.png',
+  },
+  {
+    name: 'Games librery API',
+    description: 'API para alimentar uma biblioteca de jogos pessoal!',
+    site: null,
+    github: 'https://github.com/Josimar-Souza/games-library-backend',
+    imagePath: '../../images/projects/games-library-api.png',
+  },
+  {
+    name: 'Temperature converter',
+    description: 'Uma simples site para converter temperatura celsius para fahrenheit',
+    site: 'https://temperature-converter-wjri.onrender.com/',
+    github: 'https://github.com/Josimar-Souza/Temperature-converter',
+    imagePath: '../../images/projects/temperature-converter.png',
+  },
+  {
+    name: 'Calculadora',
+    description: 'Uma simples calculadora',
+    site: 'https://calculator-8e08.onrender.com/',
+    github: 'https://github.com/Josimar-Souza/Calculator',
+    imagePath: '../../images/projects/calculator.png',
+  },
+];
+
+export default projects;
