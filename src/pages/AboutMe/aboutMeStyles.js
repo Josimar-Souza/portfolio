824dd96d511719b3dc9f996ed987d94426e86e00
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-12 13:24:17 -0300

    feat: Added mobile support to about me page

--- src/pages/AboutMe/aboutMeStyles.js
+++ src/pages/AboutMe/aboutMeStyles.js
@@ -9,6 +9,10 @@ export const AboutMeContainer = styled.div`
   justify-content: space-around;
   padding: 18px;
   width: 80%;
+
+  @media only screen and (max-width: 512px) {
+    width: 100%;
+  }
 `;
 
 export const AboutMeParagraph = styled.p`
@@ -17,15 +21,29 @@ export const AboutMeParagraph = styled.p`
   font-size: 16px;
   line-height: 25px;
   width: 80%;
+
+  @media only screen and (max-width: 512px) {
+    font-size: 14px;
+    width: 100%;
+  }
 `;
 
 export const AboutMeTitle = styled.h1`
   color: #D4ADFC;
   text-decoration: underline;
+
+  @media only screen and (max-width: 512px) {
+    margin-top: 50px;
+    margin-bottom: 20px;
+  }
 `;
 
 export const AboutMeSection = styled.h2`
   color: #D4ADFC;
+
+  @media only screen and (max-width: 512px) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const CustomDivider = styled(Divider)`
