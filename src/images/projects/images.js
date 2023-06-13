c21a2ce6c4089d3394f4d91524f48bf1c468ac7a
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-13 10:41:24 -0300

    feat: dynamic imports removed

---
+++ src/images/projects/images.js
@@ -0,0 +1,17 @@
+import calculatorImage from './calculator.png';
+import countriesInfosImage from './countries-infos.png';
+import gamesLibraryApiImage from './games-library-api.png';
+import musicLibrary from './music-library.png';
+import sharpMoviesImage from './sharp-movies.png';
+import temperatureConverter from './temperature-converter.png';
+
+const images = {
+  calculatorImage,
+  countriesInfosImage,
+  gamesLibraryApiImage,
+  musicLibrary,
+  sharpMoviesImage,
+  temperatureConverter,
+};
+
+export default images;
